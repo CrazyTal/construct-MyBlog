896382478d20fec817ece5f162a0a557b0090f9e
Wilson Wang <348627642@qq.com>
2016-07-15 17:47:55 +0800

    更新gulp任务

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 const gulp = require('gulp');
-const less = require('gulp-less'); // 编译less
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify');
+const less = require('gulp-less');           // 编译less
+const concat = require('gulp-concat'); 			 // 合并文件
+const uglify = require('gulp-uglify'); 			 // js压缩
+const minifycss = require('gulp-minify-css') // css压缩
 // const sourcemaps = require('gulp-sourcemaps');
-const babel = require('gulp-babel'); // ECMAscript 6 => ECMAscript 5
-// var del = require('del');
+const babel = require('gulp-babel'); 				 // ECMAscript 6 => ECMAscript 5
+
 
 gulp.task('testLess',() => {
   // 需要编译的less文件, 
   gulp.src(['public/less/blog.less/*.less'])
+  		.pipe(concat('blog.less'))
       .pipe(less())
       .pipe(gulp.dest('public/stylesheets/'));  // 输出目录
 })
 
+// 转译ES 6 -> ES 5
+gulp.task('translate', () => {
+	gulp.src(['public/javascripts/controller/*.js'])
+			// .pipe(concat('controller.js'))
+			.pipe(babel({
+				presets: ['es2015']
+			}))
+			.pipe(gulp.dest('public/javascripts/'))
+}) 
 // 生成sourcemap文件
 // gulp.task('testLess',() => {
 //   gulp.src('public/less/blog.less/*.less')
